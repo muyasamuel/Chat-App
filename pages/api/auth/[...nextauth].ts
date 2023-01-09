c95eb87778e7d3d66a0c91dec603a4331400bc14
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2023-01-09 13:39:00 +0300

    implement the sig in functionality

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,21 @@
+import NextAuth from "next-auth"
+import FacebookProvider from "next-auth/providers/facebook"
+
+export const authOptions = {
+
+  providers: [
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID!,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+    }),
+    // ...add more providers here   
+  ],
+  secret: process.env.NEXTAUTH_SECRET!,
+  
+  pages: {
+    signIn :"/auth/signin",
+}
+
+}
+
+export default NextAuth(authOptions)
